8e9e81e0bafa659ad292a64fbc83b9b81bf71825
Asilvazavala <asilvazavala@gmail.com>
2024-05-14 22:15:32 -0600

    added components hero, parents & place

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,13 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#00abf0",
-        secondary: "#141e33",
-        third: "#27052e",
-        fourth: "#C8B6F8",
-        fifth: "#8C2DE0",
-        gray: "#94A3B8",
-        grayStrong: "#454545ad",
+        primary: "#733713",
+        secondary: "#CB532C",
+        third: "#CD6C36",
+        gray: "#909484",
       },
     },
   },
